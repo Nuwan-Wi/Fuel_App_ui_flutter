b1924ca2f98cc82c461330e6d82eb1540130f8c2
Nuwan-Wi <nuwanwijeweera@gmail.com>
2023-01-06 20:21:15 +0530

    splash page worked

--- sign_in/lib/Splash.dart
+++ sign_in/lib/Splash.dart
@@ -1,6 +1,8 @@
+// ignore: file_names
+import 'package:animated_splash_screen/animated_splash_screen.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/src/widgets/container.dart';
-import 'package:flutter/src/widgets/framework.dart';
+// ignore: depend_on_referenced_packages
+import 'package:page_transition/page_transition.dart';
 
 import 'login.dart';
 
@@ -12,44 +14,77 @@ class Splash extends StatefulWidget {
 }
 
 class _SplashState extends State<Splash> {
-  @override
-  void initState() {
-    // TODO: implement initState
-    super.initState();
-    _navigateToHome();
-  }
 
-  _navigateToHome() async {
-    await Future.delayed(Duration(milliseconds: 1500), () {});
-    Navigator.pushReplacement(
-        context, MaterialPageRoute(builder: (context) => LoginScreen()));
-  }
-
-  @override
+@override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.brown.shade200,
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
+    return AnimatedSplashScreen(
+      splash: Container(
+        height: 600,
+        child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
           Container(
-            width: 100,
-            height: 100,
-            color: Colors.lightBlue,
-          ),
+            width: 200,
+            child: Image.asset('assets/fuel-gauge-png-fuel-1680.jpg')
+            ),
           Container(
-            child: Center(
-              child: Text(
-                'Fuel App',
-                style: TextStyle(
-                  fontSize: 20,
-                  color: Colors.white,
-                ),
+            child: const Text(
+              "Fuel App",
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 30,
+                fontWeight: FontWeight.bold,
               ),
             ),
-          ),
-        ],
+          )
+        ]),
       ),
+      //'assets/fuel-gauge-png-fuel-1680.jpg',
+      nextScreen: LoginScreen(),
+      splashTransition: SplashTransition.fadeTransition,
+      pageTransitionType: PageTransitionType.bottomToTop,
+      backgroundColor: Colors.brown.shade300,
+      duration: 4000,
+      
     );
   }
+
+  // @override
+  // void initState() {
+  //   // TODO: implement initState
+  //   super.initState();
+  //   _navigateToHome();
+  // }
+
+  // _navigateToHome() async {
+  //   await Future.delayed(Duration(milliseconds: 1500), () {});
+  //   Navigator.pushReplacement(
+  //       context, MaterialPageRoute(builder: (context) => LoginScreen()));
+  // }
+
+  // @override
+  // Widget build(BuildContext context) {
+  //   return Scaffold(
+  //     backgroundColor: Colors.brown.shade200,
+  //     body: Column(
+  //       mainAxisAlignment: MainAxisAlignment.center,
+  //       children: [
+  //         Container(
+  //           width: 100,
+  //           height: 100,
+  //           color: Colors.lightBlue,
+  //         ),
+  //         Container(
+  //           child: Center(
+  //             child: Text(
+  //               'Fuel App',
+  //               style: TextStyle(
+  //                 fontSize: 20,
+  //                 color: Colors.white,
+  //               ),
+  //             ),
+  //           ),
+  //         ),
+  //       ],
+  //     ),
+  //   );
+  // }
 }
